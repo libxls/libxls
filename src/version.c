9438096b55af6424ef391c9af8612f52c32a1815
cleit <cleit@982eeb1d-6a45-41fa-94c8-6813189e3c3d>
2008-02-23 06:13:11 +0000

    Defined C Macros for LIBXLS_(MAJOR/MINOR/MICRO)_VERSION

--- src/version.c
+++ src/version.c
@@ -1,3 +1,7 @@
-int libxls_major_version = 0;
-int libxls_minor_version = 0;
-int libxls_micro_version = 1;
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+int libxls_major_version = LIBXLS_MAJOR_VERSION;
+int libxls_minor_version = LIBXLS_MINOR_VERSION;
+int libxls_micro_version = LIBXLS_MICRO_VERSION;
