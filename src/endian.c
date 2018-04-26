libxls/libxls

commit 61a22747ac3e5328d54a0287274bd8956d9e74f5

exit on out of range failures

src/ole.c
@@ -79,11 +79,11 @@ void ole2_bufread(OLE2Stream* olest)
 			olest->pos=0;
 			olest->cfat++;
 		} else {
-
-			assert(olest->fatpos >= 0);
-
 			//printf("fatpos: %d max=%u\n",olest->fatpos, (olest->ole->cfat*olest->ole->lsector)/4);
-			if(olest->fatpos > (olest->ole->cfat*olest->ole->lsector)/4) exit(-1);
+			if(olest->fatpos > ((olest->ole->cfat*olest->ole->lsector)/4)) {
+                fprintf(stderr, "Error: fatpos out-of-bounds\n");
+                exit(-1);
+            }
 
 #if 0 // TODO: remove
 			fseek(olest->ole->file,olest->fatpos*olest->ole->lsector+512,0);
@@ -459,15 +459,17 @@ static int sector_read(OLE2* ole2, BYTE *buffer, size_t sid)
 	//printf("sector_read: sid=%zu (0x%zx) lsector=%u sector_pos=%zu\n", sid, sid, ole2->lsector, sector_pos(ole2, sid) );
     seeked = fseek(ole2->file, sector_pos(ole2, sid), SEEK_SET);
 	if(seeked != 0) {
-		printf("seek: wanted to seek to sector %zu (0x%zx) loc=%zu\n", sid, sid, sector_pos(ole2, sid));
-	}
-	assert(seeked == 0);
-	
+		fprintf(stderr, "Error: wanted to seek to sector %zu (0x%zx) loc=%zu\n", sid, sid, sector_pos(ole2, sid));
+        exit(-1);
+    }
+
 	num = fread(buffer, ole2->lsector, 1, ole2->file);
-	if(num != 1) {
-		fprintf(stderr, "fread: wanted 1 got %zu loc=%zu\n", num, sector_pos(ole2, sid));
-	}
-	assert(num == 1);
+    //assert(num == 1);
+    if(num != 1) {
+        fprintf(stderr, "Error: fread wanted 1 got %zu loc=%zu\n", num, sector_pos(ole2, sid));
+        // corrupt file
+        exit(-1);
+    }
 
     return 0;
 }
@@ -478,15 +480,21 @@ static size_t read_MSAT(OLE2* ole2, OLE2Header* oleh)
     int sectorNum;
 
     // reconstitution of the MSAT
-    ole2->SecID=malloc(ole2->cfat*ole2->lsector);
+    int count;
+    count = (ole2->cfat < 109) ? ole2->cfat : 109;
+    if(count < 0) {
+        fprintf(stderr, "Error: MSAT count out-of-bounds\n");
+        exit(-1);
+    }
+
+    size_t msize;
+    msize = count*ole2->lsector;
+    ole2->SecID=malloc(msize);
 
     // read first 109 sectors of MSAT from header
     {
-        int count;
-        count = (ole2->cfat < 109) ? ole2->cfat : 109;
         for (sectorNum = 0; sectorNum < count; sectorNum++)
         {
-			assert(sectorNum >= 0);
             sector_read(ole2, (BYTE*)(ole2->SecID)+sectorNum*ole2->lsector, oleh->MSAT[sectorNum]);
         }
     }
@@ -510,10 +518,14 @@ static size_t read_MSAT(OLE2* ole2, OLE2Header* oleh)
               //printf("   s[%d]=%d (0x%x)\n", posInSector, s, s);
 
               if (s != ENDOFCHAIN && s != FREESECT) // see patch in Bug 31. For very large files
-                {
-                 sector_read(ole2, (BYTE*)(ole2->SecID)+sectorNum*ole2->lsector, s);
-                 sectorNum++;
+              {
+                if((sectorNum*ole2->lsector + s) > msize) {
+                    fprintf(stderr, "Error: MSAT sector out-of-bounds\n");
+                    exit(-1);
                 }
+                sector_read(ole2, (BYTE*)(ole2->SecID)+sectorNum*ole2->lsector, s);
+                sectorNum++;
+              }
 			}
 			sid = *(DWORD_UA *)(sector + posInSector*4);
 			//printf("   s[%d]=%d (0x%x)\n", posInSector, sid, sid);

include/libxls/xlstool.h
@@ -47,6 +47,6 @@ extern void xls_showCell(struct st_cell_data* cell);
 extern void xls_showFont(struct st_font_data* font);
 extern void xls_showXF(XF8* xf);
 extern void xls_showFormat(struct st_format_data* format);
-extern BYTE* xls_getfcell(xlsWorkBook* pWB,struct st_cell_data* cell,DWORD *label);
+extern BYTE* xls_getfcell(xlsWorkBook* pWB,struct st_cell_data* cell, WORD *label);
 extern char* xls_getCSS(xlsWorkBook* pWB);
 extern void xls_showBOF(BOF* bof);

src/xls.c
@@ -30,6 +30,7 @@
  *
  */
 
+#define _GNU_SOURCE /* asprintf */
 #include "config.h"
 
 #include <stdio.h>
@@ -50,7 +51,7 @@
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #endif
 
-#define DEBUG_DRAWINGS
+//#define DEBUG_DRAWINGS
 int xls_debug = 0;
 
 static double NumFromRk(DWORD_UA drk);
@@ -509,7 +510,6 @@ struct st_cell_data *xls_addCell(xlsWorkSheet* pWS,BOF* bof,BYTE* buf)
 		if(formula_handler) formula_handler(bof->id, bof->size, buf);
         break;
     case XLS_RECORD_MULRK:
-printf("MULRK: %d\n", bof->size);
         for (i = 0; i < (bof->size - 6)/6; i++)	// 6 == 2 row + 2 col + 2 trailing index
         {
             cell=&row->cells.cell[xlsShortVal(((MULRK*)buf)->col + i)];
@@ -523,7 +523,12 @@ printf("MULRK: %d\n", bof->size);
     case XLS_RECORD_MULBLANK:
         for (i = 0; i < (bof->size - 6)/2; i++)	// 6 == 2 row + 2 col + 2 trailing index
         {
-            cell=&row->cells.cell[xlsShortVal(((MULBLANK*)buf)->col) + i];
+            WORD index = xlsShortVal(((MULBLANK*)buf)->col) + i;
+            if(index >= row->cells.count) {
+                fprintf(stderr, "Error: MULTI-BLANK index out of bounds\n");
+                exit(-1);
+            }
+            cell=&row->cells.cell[index];
             cell->id=XLS_RECORD_BLANK;
             cell->xf=xlsShortVal(((MULBLANK*)buf)->xf[i]);
             cell->str=xls_getfcell(pWS->workbook,cell, NULL);
@@ -531,7 +536,7 @@ printf("MULRK: %d\n", bof->size);
         break;
     case XLS_RECORD_LABELSST:
     case XLS_RECORD_LABEL:
-		cell->str=xls_getfcell(pWS->workbook,cell,(DWORD_UA *)&((LABEL*)buf)->value);
+		cell->str=xls_getfcell(pWS->workbook,cell,(WORD_UA *)&((LABEL*)buf)->value);
 		sscanf((char *)cell->str, "%d", &cell->l);
 		sscanf((char *)cell->str, "%lf", &cell->d);
 		break;
@@ -704,14 +709,28 @@ void xls_addColinfo(xlsWorkSheet* pWS,COLINFO* colinfo)
 void xls_mergedCells(xlsWorkSheet* pWS,BOF* bof,BYTE* buf)
 {
     int count=xlsShortVal(*((WORD_UA *)buf));
+    DWORD limit = 2+count*sizeof(struct MERGEDCELLS);
+    if(limit > (DWORD)bof->size) {
+        verbose("Merged Cells Count out of range");
+        return;
+    }
     int i,c,r;
-    struct MERGEDCELLS* span;
+    struct MERGEDCELLS *span;
     verbose("Merged Cells");
     for (i=0;i<count;i++)
     {
         span=(struct MERGEDCELLS*)(buf+(2+i*sizeof(struct MERGEDCELLS)));
         xlsConvertMergedcells(span);
         //		printf("Merged Cells: [%i,%i] [%i,%i] \n",span->colf,span->rowf,span->coll,span->rowl);
+        // Sanity check:
+        if(!(   span->rowf <= span->rowl &&
+                span->rowl <= pWS->rows.lastrow &&
+                span->colf <= span->coll &&
+                span->coll <= pWS->rows.lastcol
+        )) {
+            continue; // should probably exit(-1)!
+        }
+
         for (r=span->rowf;r<=span->rowl;r++)
             for (c=span->colf;c<=span->coll;c++)
                 pWS->rows.row[r].cells.cell[c].isHidden=1;
@@ -917,7 +936,7 @@ void xls_parseWorkBook(xlsWorkBook* pWB)
 		case XLS_RECORD_DEFINEDNAME:
 			if(xls_debug) {
 				int i;
-				printf("   DEFINEDNAME: ");
+                printf("   DEFINEDNAME: ");
 				for(i=0; i<bof1.size; ++i) printf("%2.2x ", buf[i]);
 				printf("\n");
 			}
@@ -971,12 +990,21 @@ void xls_preparseWorkSheet(xlsWorkSheet* pWS)
     do
     {
 		size_t read;
-        read = ole2_read(&tmp, 1,4,pWS->workbook->olestr);
-		assert(read == 4);
+        read = ole2_read(&tmp, 1, 4, pWS->workbook->olestr);
+		//assert(read == 4);
+		if(read != 4) {
+            fprintf(stderr, "Error: failed to read OLE size\n");
+            exit(-1);
+        }
         xlsConvertBof(&tmp);
         buf=(BYTE *)malloc(tmp.size);
-        read = ole2_read(buf, 1,tmp.size,pWS->workbook->olestr);
-		assert(read == tmp.size);
+        read = ole2_read(buf, 1, tmp.size, pWS->workbook->olestr);
+		//assert(read == tmp.size);
+		if(read != tmp.size) {
+            fprintf(stderr, "Error: failed to read OLE block\n");
+            exit(-1);
+        }
+
         switch (tmp.id)
         {
         case XLS_RECORD_DEFCOLWIDTH:
@@ -1059,7 +1087,10 @@ void xls_parseWorkSheet(xlsWorkSheet* pWS)
     BOF tmp;
     BYTE* buf;
 	long offset = pWS->filepos;
+    size_t read;
+#ifdef DEBUG_DRAWINGS
 	int continueRec = 0;
+#endif
 
 	struct st_cell_data *cell;
 	xlsWorkBook *pWB = pWS->workbook;
@@ -1081,10 +1112,23 @@ void xls_parseWorkSheet(xlsWorkSheet* pWS)
 		if(xls_debug > 10) {
 			printf("LASTPOS=%ld pos=%zd filePos=%d filePos=%d\n", lastPos, pWB->olestr->pos, pWS->filepos, pWB->filepos);
 		}
-        ole2_read(&tmp, 1,4,pWS->workbook->olestr);
+        read = ole2_read(&tmp, 1, 4, pWS->workbook->olestr);
+		if(read != 4) {
+            fprintf(stderr, "Error: failed to read OLE size\n");
+            exit(-1);
+        }
+
+		if(read != tmp.size) {
+            fprintf(stderr, "Error: failed to read OLE block\n");
+            exit(-1);
+        }
         xlsConvertBof((BOF *)&tmp);
         buf=(BYTE *)malloc(tmp.size);
-        ole2_read(buf, 1,tmp.size,pWS->workbook->olestr);
+        read = ole2_read(buf, 1, tmp.size, pWS->workbook->olestr);
+		if(read != tmp.size) {
+            fprintf(stderr, "Error: failed to read OLE block\n");
+            exit(-1);
+        }
 		offset += 4 + tmp.size;
 
 		if(xls_debug)

src/xls2csv.c
@@ -100,6 +100,7 @@ int main(int argc, char *argv[]) {
             break;
         }
      }
+printf("FILE: %s\n", argv[1]);
 
 	// open workbook, choose standard conversion
 	pWB = xls_open(argv[1], encoding);
@@ -149,7 +150,7 @@ int main(int argc, char *argv[]) {
 		for (j = 0; j <= (unsigned int)pWS->rows.lastrow; ++j) {
 			int isFirstCol = 1;
 			WORD cellRow = (WORD)j;
-			struct st_row_data* row = xls_row(pWS, cellRow);
+			//struct st_row_data* row = xls_row(pWS, cellRow);
 
 			// process cells
 			if (!isFirstLine) {

src/xlstool.c
@@ -30,6 +30,7 @@
  *
  */
 
+#define _GNU_SOURCE /* asprintf */
 #include "config.h"
 
 #include <math.h>
@@ -623,10 +624,11 @@ void xls_showXF(XF8* xf)
     printf("GroundColor: 0x%x\n",xf->groundcolor);
 }
 
-BYTE *xls_getfcell(xlsWorkBook* pWB,struct st_cell_data* cell,DWORD *label)
+BYTE *xls_getfcell(xlsWorkBook* pWB,struct st_cell_data* cell, WORD *label)
 {
     struct st_xf_data *xf;
 	WORD	len;
+    WORD    offset;
     char	*ret = NULL;
 
     xf=&pWB->xfs.xf[cell->xf];
@@ -635,7 +637,10 @@ BYTE *xls_getfcell(xlsWorkBook* pWB,struct st_cell_data* cell,DWORD *label)
     {
     case XLS_RECORD_LABELSST:
 		//printf("WORD: %u short: %u str: %s\n", *label, xlsShortVal(*label), pWB->sst.string[xlsIntVal(*label)].str );
-        asprintf(&ret,"%s",pWB->sst.string[xlsIntVal(*label)].str);
+        offset = xlsIntVal(*(DWORD *)label);
+        if(offset < pWB->sst.count) {
+            asprintf(&ret,"%s",pWB->sst.string[xlsIntVal(*(DWORD *)label)].str);
+        }
         break;
     case XLS_RECORD_BLANK:
     case XLS_RECORD_MULBLANK:

commit c847d632956bde62e51bf2d75f397d1535c6aac6

Restore support for >109 sectors. Fixes #17
Support for large files (50kb+) appears to have broken in
61a22747ac3e5328d54a0287274bd8956d9e74f5 due to a misunderstanding
on the MSAT limit. Up to 109 MSAT entries appear in the header,
but a much larger number can appear later in the file.

src/endian.c
@@ -242,9 +242,9 @@ void xlsConvertFormulaArray(FARRAY *f)
 void xlsConvertHeader(OLE2Header *h)
 {
     int i;
-    for (i=0; i<2; i++)
+    for (i=0; i<sizeof(h->id)/sizeof(h->id[0]); i++)
         h->id[i] = xlsIntVal(h->id[i]);
-    for (i=0; i<4; i++)
+    for (i=0; i<sizeof(h->clid)/sizeof(h->clid[0]); i++)
         h->clid[i] = xlsIntVal(h->clid[i]);
     h->verminor  = xlsShortVal(h->verminor);
     h->verdll    = xlsShortVal(h->verdll);
@@ -265,7 +265,7 @@ void xlsConvertHeader(OLE2Header *h)
     h->csfat = xlsIntVal(h->csfat);
     h->difstart = xlsIntVal(h->difstart);
     h->cdif = xlsIntVal(h->cdif);
-    for (i=0; i<109; i++)
+    for (i=0; i<sizeof(h->MSAT)/sizeof(h->MSAT[0]); i++)
         h->MSAT[i] = xlsIntVal(h->MSAT[i]);
 }
 

src/ole.c
@@ -613,7 +613,7 @@ static ssize_t read_MSAT_header(OLE2* ole2, OLE2Header* oleh, int sectorCount) {
     ssize_t bytes_read = 0, total_bytes_read = 0;
     int sectorNum;
 
-    for (sectorNum = 0; sectorNum < sectorCount; sectorNum++)
+    for (sectorNum = 0; sectorNum < sectorCount && sectorNum < 109; sectorNum++)
     {
         if ((bytes_read = sector_read(ole2, sector, oleh->MSAT[sectorNum])) == -1) {
             if (xls_debug) fprintf(stderr, "Error: Unable to read sector #%d\n", oleh->MSAT[sectorNum]);
@@ -730,7 +730,7 @@ cleanup:
 static ssize_t read_MSAT(OLE2* ole2, OLE2Header* oleh)
 {
     // reconstitution of the MSAT
-    int count = (ole2->cfat < 109) ? ole2->cfat : 109;
+    int count = ole2->cfat;
     if(count <= 0) {
         if (xls_debug) fprintf(stderr, "Error: MSAT count out-of-bounds\n");
         return -1;